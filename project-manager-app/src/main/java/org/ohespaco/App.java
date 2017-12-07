b637589c26680c0efca9fb349f5b726787cfe857
OhEsPaco <fcomanuel.garcia4@alu.uclm.es>
2017-12-07 18:52:33 +0100

    Creacion de clases principales

--- project-manager-app/src/main/java/org/ohespaco/App.java
+++ project-manager-app/src/main/java/org/ohespaco/App.java
@@ -1,5 +1,7 @@
 package org.ohespaco;
 
+import org.ohespaco.presentacion.MainFrame;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        MainFrame m=new MainFrame();
+        m.setVisible(true);
     }
 }
