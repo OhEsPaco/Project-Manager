9dba06a535695dd501e3f927496053e790134364
OhEsPaco <pacogrc5@gmail.com>
2018-01-08 08:09:53 +0100

    Cambio menor

--- project-manager-app/src/main/java/org/ohespaco/presentacion/JPanelLogin.java
+++ project-manager-app/src/main/java/org/ohespaco/presentacion/JPanelLogin.java
@@ -110,7 +110,7 @@ public class JPanelLogin extends JPanel {
 		loginPane.add(btnEntrar);
 
 		passwordField = new JPasswordField();
-		passwordField.setText("12345678"); //$NON-NLS-1$
+		
 	
 		passwordField.addMouseListener(new MouseAdapter() {
 			@Override
@@ -123,7 +123,7 @@ public class JPanelLogin extends JPanel {
 		loginPane.add(passwordField);
 
 		emailField = new JTextField();
-		emailField.setText("pacogrc5@gmail.com"); //$NON-NLS-1$
+		
 		emailField.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
